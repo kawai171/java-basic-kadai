d9dfd86d5eafb23135436adb1091c83bd0d5f98f
kawai171 <tkawai@aicom.co.jp>
2026-04-21 11:39:33 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		KatoTaro_Chapter18 Taro = new KatoTaro_Chapter18();
+	
+		
+		hanako.setGivenName() ;
+		ichiro.setGivenName();
+		Taro.setGivenName();
+		
+		hanako.execIntroduce() ;
+		ichiro.execIntroduce();
+		Taro.execIntroduce();
+		
+	}
+	
+	
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,28 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+	
+    //姓を表す
+	public String familyName = "加藤"; 
+	
+	
+	//名を表す
+	public String givenName;
+	
+	//住所を表す
+	public String address = "東京都中野区〇×";
+	
+	public void commonIntroduce() {  
+		System.out.println("名前は"+ familyName + givenName +"です");
+		System.out.println("住所は" + address + "です");
+	}
+	
+	abstract public void eachIntroduce() ;
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+
+	
+}
