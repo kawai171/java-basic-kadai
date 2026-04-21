c0f83e603d5c6b5931b4c56f7c1b963949c1bcc6
kawai171 <tkawai@aicom.co.jp>
2026-04-21 14:40:42 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,15 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Dictionary_Chapter21 hanako = new Dictionary_Chapter21();
+		hanako.addWords();
+		hanako.search("apple");
+		hanako.search("banana");
+	    hanako.search("grape");
+	    hanako.search("orange");
+
+}
+}
---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,42 @@
+package kadai_021;
+import java.util.HashMap;
+
+public class Dictionary_Chapter21 {
+	
+	HashMap<String , String> dictionary = new HashMap<String,String>();
+    
+	public void addWords() { 
+		dictionary.put ("apple" ," りんご");
+		
+		dictionary.put ("peach" ," 桃");
+		
+		dictionary.put ("banana","バナナ");
+		dictionary.put ("lemon" , "レモン");
+		dictionary.put ("pear" , "梨");
+		dictionary.put ("kiwi" , "キウィ");
+		dictionary.put ("strawberry" , "いちご");
+		dictionary.put ( "grape" , "ぶどう" );
+		dictionary.put ("muscat","マスカット");
+		dictionary.put ("cherry ","さくらんぼ");
+	}
+	
+	public void search(String fruit) {
+		String tmp =dictionary.get( fruit );
+		if(tmp == null ) {
+			
+			System.out.println(fruit + "は辞書に存在しません" );
+			
+		} else {
+			
+			System.out.println(fruit + "の意味は" + dictionary.get( fruit) );
+			
+		}
+		
+		
+
+		
+	}
+	
+	
+	
+}
